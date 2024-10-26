938bd1d74d5e43a755e0183c5cbf785e38f25761
Sasha <skv43r@gmail.com>
2024-10-26 14:51:51 +0300

    feat: add function to trailing 0 in the end of factorial

---
+++ FactorialTrailingZeroes.py
@@ -0,0 +1,11 @@
+
+
+class Solution:
+    def trailingZeroes(self, n: int) -> int:
+        count = 0
+        while n > 0:
+            n //= 5
+            count += n
+        return count
+
+print(Solution().trailingZeroes(17))
