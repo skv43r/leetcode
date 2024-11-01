461d6ca5bcec68fe9dd67e3ec5e99689ebe15270
Alexandr <skv43r@gmail.com>
2024-11-01 12:57:15 +0300

    feat: add function to reverse words in string

---
+++ ReverseWordsinaString.py
@@ -0,0 +1,34 @@
+# Given an input string s, reverse the order of the words.
+# A word is defined as a sequence of non-space characters. 
+# The words in s will be separated by at least one space.
+# Return a string of the words in reverse order concatenated by a single space.
+# Note that s may contain leading or trailing spaces or multiple spaces between
+# two words. The returned string should only have a single space separating the
+# words. Do not include any extra spaces.
+
+# Example 1:
+# Input: s = "the sky is blue"
+# Output: "blue is sky the"
+
+# Example 2:
+# Input: s = "  hello world  "
+# Output: "world hello"
+# Explanation: Your reversed string should not contain leading or trailing
+# spaces.
+
+# Example 3:
+# Input: s = "a good   example"
+# Output: "example good a"
+# Explanation: You need to reduce multiple spaces between two words to a single
+# space in the reversed string.
+
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        reverse = []
+        words = s.split()
+        for i in range(len(words)):
+            word = words.pop()
+            reverse.append(word)
+        return " ".join(reverse)
+
+print(Solution().reverseWords("the sky is blue"))
