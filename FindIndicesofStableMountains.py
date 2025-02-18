b3b8f734dee7804b95b678d873f7550b7885d278
Alexandr <skv43r@gmail.com>
2025-02-18 21:45:59 +0300

    feat(FindIndicesofStableMountains): create function to Find Indices of Stable Mountains

---
+++ FindIndicesofStableMountains.py
@@ -0,0 +1,28 @@
+# There are n mountains in a row, and each mountain has a height.
+# You are given an integer array height where height[i] represents the height of
+# mountain i, and an integer threshold.
+
+# A mountain is called stable if the mountain just before it (if it exists) has
+# a height strictly greater than threshold. Note that mountain 0 is not stable.
+
+# Return an array containing the indices of all stable mountains in any order.
+
+# Example 1:
+# Input: height = [1,2,3,4,5], threshold = 2
+# Output: [3,4]
+# Explanation:
+# Mountain 3 is stable because height[2] == 3 is greater than threshold == 2.
+# Mountain 4 is stable because height[3] == 4 is greater than threshold == 2.
+
+from typing import List
+
+
+class Solution:
+    def stableMountains(self, height: List[int], threshold: int) -> List[int]:
+        mount = []
+        for i in range(1, len(height)):
+            if height[i-1] > threshold:
+                mount.append(i)
+        return mount
+    
+print(Solution().stableMountains([1,2,3,4,5], 2))
